b85bc17837cc8f2f5f713acd2ec121e553a881b3
hut <hut@hut.pm>
2021-04-11 14:11:18 +0200

    Add passthrough arduino program to generate training data

---
+++ 3_neural_network/DumbPipe.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.print("A0:");
+  Serial.print(analogRead(A0));
+  Serial.print("\tA1:");
+  Serial.print(analogRead(A1));
+  Serial.print("\tA2:");
+  Serial.print(analogRead(A2));
+  Serial.print("\tA3:");
+  Serial.print(analogRead(A3));
+  Serial.print("\tA4:");
+  Serial.print(analogRead(A4));
+  Serial.print("\tA5:");
+  Serial.print(analogRead(A5));
+  Serial.print("\tA6:");
+  Serial.print(analogRead(A6));
+  Serial.print("\tA7:");
+  Serial.print(analogRead(A7));
+  Serial.println();
+}
