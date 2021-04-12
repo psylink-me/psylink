264803d649850d6f362eeca9b8f735b1c830f538
hut <hut@hut.pm>
2021-04-12 17:16:24 +0200

    normalize electrode sensor readings to [0,1]
    I don't know if this is even necessary, but hey, let's add a bunch of
    unnecessary FLOPS to our main loop for elegance purposes. (I may revert
    this later...)

--- 3_neural_network/DumbPipe.ino
+++ 3_neural_network/DumbPipe.ino
@@ -1,3 +1,5 @@
+#define CAP 4095.0
+
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12);
@@ -5,20 +7,20 @@ void setup() {
 
 void loop() {
   Serial.print("A0:");
-  Serial.print(analogRead(A0));
+  Serial.print(analogRead(A0) / CAP);
   Serial.print("\tA1:");
-  Serial.print(analogRead(A1));
+  Serial.print(analogRead(A1) / CAP);
   Serial.print("\tA2:");
-  Serial.print(analogRead(A2));
+  Serial.print(analogRead(A2) / CAP);
   Serial.print("\tA3:");
-  Serial.print(analogRead(A3));
+  Serial.print(analogRead(A3) / CAP);
   Serial.print("\tA4:");
-  Serial.print(analogRead(A4));
+  Serial.print(analogRead(A4) / CAP);
   Serial.print("\tA5:");
-  Serial.print(analogRead(A5));
+  Serial.print(analogRead(A5) / CAP);
   Serial.print("\tA6:");
-  Serial.print(analogRead(A6));
+  Serial.print(analogRead(A6) / CAP);
   Serial.print("\tA7:");
-  Serial.print(analogRead(A7));
+  Serial.print(analogRead(A7) / CAP);
   Serial.println();
 }
