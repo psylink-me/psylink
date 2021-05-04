ef525841f09f699495b6f2641e41a2b3e4b753d2
hut <hut@hut.pm>
2021-05-04 07:25:12 +0200

    Boost BLE rate from 1kb/s to 6-7kb/s (on my setup)
    GNURadio only runs with ~3kb/s due to overhead
    
    This was mainly accomplished by lowering the BLE connection interval to
    ~10ms.  Unfortunately it eats up considerably more power now.  Got the
    idea thanks to this comment:
    
    https://devzone.nordicsemi.com/f/nordic-q-a/7620/ble-gatt-read-characteristic-slow-or-normal/27191#27191

--- 5_ble/arduino/BLEpipe2/BLEpipe2.ino
+++ 5_ble/arduino/BLEpipe2/BLEpipe2.ino
@@ -1,10 +1,12 @@
 #include <ArduinoBLE.h>
-#include "NRF52_MBED_TimerInterrupt.h"
+//#include "NRF52_MBED_TimerInterrupt.h"
 
 // Configuration
-#define SAMPLE_RATE 250 // samples per second
-#define BLE_NOTIFY_RATE 2 // updates per second
-#define CHANNELS 4
+#define SAMPLE_RATE 500 // samples per second
+#define BLE_NOTIFY_RATE 10 // updates per second
+#define BLE_CONNECTION_INTERVAL_MIN 8 // in steps of 1.25ms
+#define BLE_CONNECTION_INTERVAL_MAX 8 // in steps of 1.25ms
+#define CHANNELS 8
 #define BUFFERS 2 // multiple buffers help with concurrency issues, if needed
 #define METADATA_BYTES 1
 // Metadata format:
@@ -20,7 +22,7 @@ const int SAMPLES_PER_INTERVAL = SAMPLE_RATE / BLE_NOTIFY_RATE;
 const int BLE_CHARACTERISTIC_SIZE = METADATA_BYTES + CHANNELS * SAMPLES_PER_INTERVAL;
 const int NO_BUFFER = -1;
 
-NRF52_MBED_Timer samplingTimer(NRF_TIMER_3);
+//NRF52_MBED_Timer samplingTimer(NRF_TIMER_3);
 BLEDevice connectedDevice;
 BLEService sensorService("0a3d3fd8-2f1c-46fd-bf46-eaef2fda91e4");
 BLEStringCharacteristic sensorCharacteristic("0a3d3fd8-2f1c-46fd-bf46-eaef2fda91e5", BLERead, BLE_CHARACTERISTIC_SIZE);
@@ -33,6 +35,7 @@ int currentSample = 0;
 int currentBuffer = 0;
 unsigned char tick = 1;
 char bleString[BLE_CHARACTERISTIC_SIZE] = {0};
+bool bleConnected = false;
 
 void setup() {
   //Serial.begin(115200);
@@ -48,12 +51,12 @@ void setup() {
     digitalWrite(LEDR, LOW); // Turn on red LED
     while (1);
   }
-  if (!samplingTimer.attachInterruptInterval(SAMPLE_INTERVAL_uS, samplingTimerHandler)) {
-    digitalWrite(LEDR, LOW); // Turn on red LED
-    digitalWrite(LEDG, LOW); // Turn on green LED
-    while (1);
-  }
-  samplingTimer.stopTimer();
+//  if (!samplingTimer.attachInterruptInterval(SAMPLE_INTERVAL_uS, samplingTimerHandler)) {
+//    digitalWrite(LEDR, LOW); // Turn on red LED
+//    digitalWrite(LEDG, LOW); // Turn on green LED
+//    while (1);
+//  }
+  //samplingTimer.stopTimer();
   BLE.setLocalName("Myocular");
   BLE.setAdvertisedService(sensorService);
   sensorService.addCharacteristic(sensorCharacteristic);
@@ -61,16 +64,23 @@ void setup() {
   BLE.addService(sensorService);
   BLE.setEventHandler(BLEConnected, bleConnectHandler);
   BLE.setEventHandler(BLEDisconnected, bleDisconnectHandler);
-  sensorCharacteristic.setEventHandler(BLERead, sensorCharacteristicRead);
+  //sensorCharacteristic.setEventHandler(BLERead, sensorCharacteristicRead);
   sensorCharacteristic.writeValue("0");
   channelCountCharacteristic.writeValue(CHANNELS);
+  BLE.setConnectionInterval(BLE_CONNECTION_INTERVAL_MIN, BLE_CONNECTION_INTERVAL_MAX);
   BLE.advertise();
   for (int i = 0; i < BLE_CHARACTERISTIC_SIZE; i++) { bleString[i] = i+1; }
 }
 
+unsigned long nextFrame = 0;
 void loop() {
-  if (doSampling) {
+  //if (doSampling) {
+  if (micros() >= nextFrame) {
     readSamples();
+    nextFrame = micros() + 1000;
+  }
+  if (sendBuffer != NO_BUFFER) {
+    updateSensorCharacteristic();
   }
   BLE.poll();
 }
@@ -100,7 +110,7 @@ void readSamples() {
 }
 
 void updateSensorCharacteristic() {
-  if (sendBuffer == NO_BUFFER) return;
+  if (sendBuffer == NO_BUFFER || !bleConnected) return;
   int pos = 0;
   char currentChar;
 
@@ -123,6 +133,7 @@ void updateSensorCharacteristic() {
 void bleConnectHandler(BLEDevice central) {
   digitalWrite(LEDB, LOW);
   connectedDevice = central;
+  bleConnected = true;
   currentSample = 0;
   currentBuffer = 0;
   tick = 1;
@@ -131,10 +142,11 @@ void bleConnectHandler(BLEDevice central) {
     for (int channel = 0; channel < CHANNELS; channel++)
       for (int sample = 0; sample < SAMPLES_PER_INTERVAL; sample++)
         samples[buf][channel][sample] = 0;
-  samplingTimer.restartTimer();
+  //samplingTimer.restartTimer();
 }
 
 void bleDisconnectHandler(BLEDevice central) {
-  samplingTimer.stopTimer();
+  //samplingTimer.stopTimer();
   digitalWrite(LEDB, HIGH);
+  bleConnected = false;
 }
--- 5_ble/gnuradio/block_BLE_source.py
+++ 5_ble/gnuradio/block_BLE_source.py
@@ -6,7 +6,7 @@ import importlib
 import os.path
 import sys
 from threading import Thread, Event
-from queue import Queue
+from queue import SimpleQueue
 from gnuradio import gr
 
 # NOTE: fill in the path to the pymyocular library
@@ -21,42 +21,45 @@ USE_BLE = True
 
 def run_bluetooth_loop(device, sample_pipe, stop_thread):
     decoder = pymyocular.BLEDecoder()
-    fps = 0
+    sps = 0
     bps = 0
     last_tick = None
-    nextfps = time.time() + 1
+    nextsps = time.time() + 1
     device.connect()
     try:
         decoder.decode_channel_count(device.char_read(pymyocular.BLEDevice.OPTION_CHANNELS_UUID))
         while not stop_thread.is_set():
+            t = time.time()
             read = device.char_read(pymyocular.BLEDevice.SENSOR_UUID)
-            print('[BLE] Received a packet of length %d' % len(read))
+            t = time.time() - t
             data = decoder.decode_packet(read)
+            #print('[BLE] Received a packet of length %d, duration: %f, tick: %d' % (len(read), t, data['tick']))
             samples = data['samples']
             tick = data['tick']
 
             if last_tick is not None:
                 if last_tick == tick:
                     # This packet has already been received
-                    print('Dropped packet, it has already been received')
+                    #print('Dropped packet, it has already been received')
                     continue
 
                 # Need to consider overflow of tick value. It's range is between 1 and incl. 255
-                lost_packets = min(tick - last_tick - 1, tick + 255 - last_tick - 1)
-                null_sample = [0] * data['channels']
-                for _ in range(lost_packets * len(samples)):
-                    sample_pipe.put(null_sample)
+                #lost_packets = min(max(0, tick - last_tick - 1), tick + 255 - last_tick - 1)
+                #if lost_packets:
+                #    null_sample = np.zeros((data['channels'], lost_packets * len(samples)))
+                #    sample_pipe.put(null_sample)
             last_tick = tick
 
-            for sample in samples:
-                sample_pipe.put(sample)
+            #for sample in samples:
+                #sample_pipe.put(sample)
+            sample_pipe.put(samples)
 
             bps += len(read)
-            fps += len(samples)
-            if time.time() >= nextfps:
-                print(f"FPS: {fps}, BPS: {bps}")
-                nextfps += 1
-                fps = 0
+            sps += len(samples)
+            if time.time() >= nextsps:
+                print(f"SPS: {sps}, BPS: {bps}")
+                nextsps += 1
+                sps = 0
                 bps = 0
     finally:
         device.disconnect()
@@ -66,26 +69,24 @@ class BLESource(gr.basic_block):
     def __init__(self, ble_mac=DEFAULT_BLE_ADDRESS):
         gr.basic_block.__init__(self, name='BLE Source', in_sig=[], out_sig=[np.float32] * SIGNAL_COUNT)
         self.ble_mac = ble_mac
-        self.sample_pipe = Queue()
+        self.sample_pipe = SimpleQueue()
         self.stop_thread = Event()
+        self.last_sample_count = 2048  # some large conservative value
         self.device = BLE_GATT.Central(ble_mac)
         self._bt_thread = None
 
     def general_work(self, input_items, output_items):
         count = 0
         if USE_BLE:
-            for i in range(min(100, len(output_items[0]))):
-                if self.sample_pipe.empty():
-                    break
-                channels = self.sample_pipe.get()
-                for channel, samples in enumerate(channels):
-                    try:
-                        output_channel = output_items[channel]
-                    except IndexError:
-                        continue
-                    else:
-                        output_channel[i] = samples
-                count += 1
+            limit = len(output_items[0]) - self.last_sample_count
+            while not self.sample_pipe.empty() and count < limit:
+                samples = self.sample_pipe.get()
+                transposed = np.transpose(samples)
+                for channel_id, channel in enumerate(output_items):
+                    channel[count:count+len(samples)] = transposed[channel_id]
+                count += len(samples)
+                self.last_sample_count = len(samples)
+                limit = len(output_items[0]) - self.last_sample_count
         else:
             for i in range(min(100, len(output_items[0]))):
                 output_items[0][i] = math.sin(i)
