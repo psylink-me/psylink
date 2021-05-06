dac3ec5ac33c7ee1ddda9ecb23822650df0029eb
hut <hut@hut.pm>
2021-05-06 03:01:46 +0200

    add pylint -E to test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# 1. Run doctests for all files in the pyocular directory that contain "import doctest"
+echo '=== 1. Doctests ==='
 find pyocular -type f -iname '*.py' \
     -exec grep -lq 'import doctest' '{}' ';' \
     -exec echo 'Running doctest for {}' ';' \
     -exec python '{}' ';'
+
+echo '=== 2. pylint -E ==='
+pylint -E pyocular
