47143dcecd71ef3b85a77053d399a9f84ca17a20
hut <hut@hut.pm>
2021-05-19 03:00:49 +0200

    support for a few selected special characters
    not all, because that produced weird behavior where keys appeared to be
    permanently pressed

--- python/psylink/keycapturer.py
+++ python/psylink/keycapturer.py
@@ -40,8 +40,20 @@ import doctest
 import logging
 import pynput.keyboard
 import time
+from pynput.keyboard import Key
 
 MAX_HISTORY_SIZE = 32
+
+SPECIAL_KEYS = {
+    Key.space: "<space>",
+    Key.backspace: "<backspace>",
+    Key.enter: "<enter>",
+    Key.left: "<left>",
+    Key.right: "<right>",
+    Key.up: "<up>",
+    Key.down: "<down>",
+}
+SPECIAL_KEYS_INV = dict((value, key) for key, value in SPECIAL_KEYS.items())
 
 class KeyCapturer:
     def __init__(self, on_change_callback=None):
@@ -98,12 +110,16 @@ class KeyCapturer:
 
     @staticmethod
     def key_to_str(key):
+        if key in SPECIAL_KEYS:
+            return SPECIAL_KEYS[key]
         if hasattr(key, 'char') and key.char:
             return key.char
         return None
 
     @staticmethod
     def str_to_key(s):
+        if s in SPECIAL_KEYS_INV[s]:
+            return SPECIAL_KEYS_INV[s]
         return pynput.Key.from_char(s)
 
     def reset(self):
@@ -135,10 +151,11 @@ class KeyPresser:
 
     def set_pressed_keys(self, keys):
         keys = set(keys) - set([''])
+
         for key in self.keys_pressed - keys:
-            self.controller.release(key)
+            self.controller.release(SPECIAL_KEYS_INV.get(key, key))
         for key in keys - self.keys_pressed:
-            self.controller.press(key)
+            self.controller.press(SPECIAL_KEYS_INV.get(key, key))
         self.keys_pressed = keys
 
 
