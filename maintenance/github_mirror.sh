0f3aa3d5bb27dc3d00c63ea184026044c243ab62
hut <hut@hut.pm>
2023-04-10 11:13:04 +0200

    add script for mirroring codeberg to github

---
+++ maintenance/github_mirror.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# This script helps with mirroring the git repositories from codeberg to github.
+#
+# usage: ./github_mirror.sh [--init] TARGET_PATH
+#
+# With "--init", it clones the relevant repositories into a folder defined by
+# TARGET_PATH so that you can later use this script to mirror them.
+#
+# Without "--init", it performs the actual mirroring.
+
+set -e
+
+REPOS="psylink psylink.wiki psylink-web psylink-3dprint psylink-datasets"
+
+# Handle arguments
+if [ "$1" = "--init" ]; then
+    action=initialize
+    shift
+else
+    action=update
+fi
+
+TARGET_PATH="$1"
+if [ -z "$TARGET_PATH" ]; then
+    echo "usage: $0 [--init] TARGET_PATH"
+    exit 1
+fi
+
+# Perform desired action
+if [ $action = initialize ]; then
+    mkdir -p "$TARGET_PATH"
+    cd "$TARGET_PATH"
+
+    for repo in $REPOS; do
+        git clone --mirror https://codeberg.org/psylink/"$repo".git
+        cd "$repo".git
+        git remote set-url --push origin git@github.com:psylink-me/"$repo".git
+        cd ..
+    done
+elif [ $action = update ]; then
+    if [ ! -d "$TARGET_PATH" ]; then
+        echo "ERROR: Target path '$TARGET_PATH' doesn't exist."
+        echo "Please run this script with the '--init' parameter first."
+        exit 1
+    fi
+    cd "$TARGET_PATH"
+    for repo in $REPOS; do
+        cd "$repo".git
+        git fetch -pP origin
+        git push --mirror
+        cd ..
+    done
+fi
